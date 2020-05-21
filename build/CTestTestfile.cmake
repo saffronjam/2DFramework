3497e23269d21bee47fdfb954bfff2efb97e7d5f
viesa <own.emka@gmail.com>
2020-05-21 20:33:51 +0200

    Rework base

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/viesa/repos/V-2DFramework
+# Build directory: /home/viesa/repos/V-2DFramework/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
