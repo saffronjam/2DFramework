32a2be3d6e4d3b32316381ad0c79948e395c5daa
viesa <own.emka@gmail.com>
2020-05-24 18:35:38 +0200

    Remove folder "build" from git

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/viesa/repos/V-2DFramework
-# Build directory: /home/viesa/repos/V-2DFramework/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
